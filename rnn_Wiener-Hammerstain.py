2ae437460afe48a89a7fefebe7342a489c8848a3
Dario Cazzani <dcazzani@cisco.com>
2017-10-10 19:21:19 -0700

    Approximate the non linear static part with an nth order polynomial. Also approximate the nth order polynomial with a shallow ANN

--- rnn_Wiener-Hammerstain.py
+++ rnn_Wiener-Hammerstain.py
@@ -4,68 +4,83 @@ import matplotlib.pyplot as plt
 from logistic_map import LogisticMapGenerator, next_sample
 
 RATE = 2.5
-SEQUENCE_LENGTH = 3
-LEARNING_RATE = 1E-3
+SEQUENCE_LENGTH = 5
+LEARNING_RATE = 1E-4
 BATCH_SIZE = 128
+ORDER = 4
+APPROXIMATION_EXPONENT = 3
 
-def f_x(x, w1, w2, w3):
-    h1 = tf.nn.relu(tf.matmul(x, w1))
-    h2 = tf.nn.relu(tf.matmul(h1, w2))
-    h3 = tf.matmul(h2, w3)
-    return h3
+"""
+Approximate f_u --> Universal approximation theorem
+https://en.wikipedia.org/wiki/Universal_approximation_theorem
+a feed-forward network with a single hidden layer containing a finite number
+of neurons (i.e., a multilayer perceptron), can approximate continuous
+functions on compact subsets of R^n
+"""
+# Assuming order APPROXIMATION_EXPONENT is enough parameters
+def approx_f_u(u, w1_approx, w2_approx, order):
+	x_p = []
+	for p in range(ORDER):#np.power(order, APPROXIMATION_EXPONENT)):
+		x_p.append(u)
+		x = tf.squeeze(tf.stack(x_p, axis=1))
+	h1 = tf.nn.sigmoid(tf.matmul(x, w1_approx))
+	return tf.matmul(h1, w2_approx)
 
-# def f_x(x, rate):
-#     return rate * x * (1. - x)
+def f_u(u, w1, order):
+	# build nth order polynomial
+	x_p = []
+	for p in range(order):
+		x_p.append(tf.pow(u, p+1))
+		x = tf.squeeze(tf.stack(x_p, axis=1))
+	return tf.matmul(x, w1)
 
 def main():
-    generator = LogisticMapGenerator(RATE, SEQUENCE_LENGTH)
-    x = tf.placeholder(tf.float32, shape=[None, 1])
-    y = tf.placeholder(tf.float32, shape=[None, SEQUENCE_LENGTH])
-    w1 = tf.get_variable("w1", [1, 16], dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer())
-    w2 = tf.get_variable("w2", [16, 16], dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer())
-    w3 = tf.get_variable("w3", (16, 1), dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer())
-    rate = tf.get_variable("rate", [1], dtype=tf.float32, initializer=tf.constant_initializer(0.1))
+	generator = LogisticMapGenerator(RATE, SEQUENCE_LENGTH)
+	u = tf.placeholder(tf.float32, shape=[None, 1])
+	y = tf.placeholder(tf.float32, shape=[None, SEQUENCE_LENGTH])
+	w1 = tf.get_variable("w1", [ORDER, 1], dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer())
+	w1_approx = tf.get_variable("w1_approx", [ORDER, ORDER**APPROXIMATION_EXPONENT], dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer())
+	w2_approx = tf.get_variable("w2_approx", [ORDER**APPROXIMATION_EXPONENT, 1], dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer())
 
-    with tf.variable_scope('memoryless_static_rnn') as scope:
-        cells = []
-        cells.append(x)
-        for idx, _ in enumerate(range(SEQUENCE_LENGTH - 1)):
-            if idx == 0:
-                # cell = f_x(x, rate)
-                cell = f_x(x, w1, w2, w3)
-            else:
-                # cell = f_x(cell, rate)
-                cell = f_x(cell, w1, w2, w3)
-            cells.append(cell)
-            scope.reuse_variables()
+	with tf.variable_scope('memoryless_static_rnn') as scope:
+		cells = []
+		cells.append(u)
+		for idx, _ in enumerate(range(SEQUENCE_LENGTH - 1)):
+			if idx == 0:
+				# cell = f_u(u, w1, ORDER)
+				cell = approx_f_u(u, w1_approx, w2_approx, ORDER)
+			else:
+				# cell = f_u(cell, w1, ORDER)
+				cell = approx_f_u(cell, w1_approx, w2_approx, ORDER)
+			cells.append(cell)
+			scope.reuse_variables()
 
-    cells = tf.stack(cells, axis=1)
-    loss = tf.reduce_mean(tf.square(tf.squeeze(tf.stack(cells)) - y))
-    training_step = tf.train.AdamOptimizer(LEARNING_RATE).minimize(loss)
+	cells = tf.stack(cells, axis=1)
+	loss = tf.reduce_mean(tf.square(tf.squeeze(tf.stack(cells)) - y))
+	training_step = tf.train.AdamOptimizer(LEARNING_RATE).minimize(loss)
 
-    with tf.Session() as sess:
-        # TRAIN
-        sess.run(tf.global_variables_initializer())
-        for i in range(10000):
-            _input, _sequence = generator.sample(BATCH_SIZE)
-            # _, loss_val, rate_val = sess.run([training_step, loss, rate], feed_dict={x: _input, y: _sequence})
-            _, loss_val = sess.run([training_step, loss], feed_dict={x: _input, y: _sequence})
-            if i % 200 == 0:
-                # print('loss: {} - rate: {}\n'.format(loss_val, rate_val))
-                print('loss: {}\n'.format(loss_val))
+	with tf.Session() as sess:
+		# TRAIN
+		sess.run(tf.global_variables_initializer())
+		for i in range(20000):
+			_input, _sequence = generator.sample(BATCH_SIZE)
+			# _, loss_val, rate_val = sess.run([training_step, loss, rate], feed_dict={u: _input, y: _sequence})
+			_, loss_val = sess.run([training_step, loss], feed_dict={u: _input, y: _sequence})
+			if i % 200 == 0:
+				# print('loss: {} - rate: {}\n'.format(loss_val, rate_val))
+				print('loss: {}\n'.format(loss_val))
 
-        # TEST
-        value = 0.8
-        _input_test = np.array([value])[None, :]
-        output_rnn = sess.run(cells, feed_dict={x: _input_test})
-        output_lmap = next_sample(value, RATE, SEQUENCE_LENGTH)
+		# TEST
+		value = 0.5
+		output_rnn = sess.run(cells, feed_dict={u: np.asarray([[value], [value]])})
+		output_lmap = next_sample(value, RATE, SEQUENCE_LENGTH)
 
-        plt.plot(np.squeeze(output_rnn))
-        plt.plot(np.squeeze(output_lmap))
-        plt.legend(['rnn output', 'logistic map output'], loc='lower right')
-        # print('Estimanted rate: {} - Real rate: {}'.format(rate.eval(), RATE))
-        plt.show()
+		plt.plot(np.squeeze(output_rnn[0,:]))
+		plt.plot(np.squeeze(output_lmap))
+		plt.legend(['rnn output', 'logistic map output'], loc='lower right')
+		# print('Estimanted rate: {} - Real rate: {}'.format(rate.eval(), RATE))
+		plt.show()
 
 
 if __name__ == '__main__':
-    main()
+	main()
